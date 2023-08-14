d01b023bcf6daa4407b82d0bf08cb12fa0de528a
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-08-14 16:21:07 +0700

    menambahkan  contoh latihan 2

--- Aplikasi-Login-02-solution-study-case/assets/scripts/login-script.js
+++ Aplikasi-Login-02-solution-study-case/assets/scripts/login-script.js
@@ -13,17 +13,13 @@ const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
-    event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-  
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
+  event.preventDefault();
 
-    if (email == expectedEmail && password == expectedPassword) {
-      goToHome();
-    } else {
-      showPopUp();
-    }
+  const email = inputEmailElement.value;
+  const password = inputPasswordElement.value;
+  if (email == expectedEmail && password == expectedPassword) {
+    goToHome();
+  } else {
+    showPopUp();
+  }
 });
